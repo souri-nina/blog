76ca467b7fa5886087f3d299f6dfc1dbb53537a8
souri.nina@gmail.com <snmoukdahan@gmail.com>
2018-08-30 12:21:42 -0600

    update controller methods/views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save 
+      redirect_to pages_path(@page.id)
+    else 
+      render :new
+    end
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+
+end
